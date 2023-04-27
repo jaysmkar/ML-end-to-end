1bb18a13d406caa8d623110c86c6be4902f3ca68
jaysmkar <jayeshmandavkar5042@gmail.com>
2023-04-27 17:00:03 +0530

    setup.py file updated

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup 
+from typing import List 
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('/n','') for req in requirements]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements 
+setup(
+    name = 'Machine Learning Project',
+    author = 'Jayesh',
+    author_email = 'jayeshmandavkar5042@gmail.com',
+    version = '0.0.1',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+# this file is created just because the machine learning project is featcjed as the package for the installation 
+
