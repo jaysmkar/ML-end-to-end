02b62bd091cec7a7577c032d8194e9d0e4a56145
jaysmkar <jayeshmandavkar5042@gmail.com>
2023-05-08 11:54:53 +0530

    final update in the setup.py file

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
-# importing the dependencies 
-from setup import setuptools, find_packages 
+from setup import setuptools, find_packages
 from typing import List 
 
 HYPHEN_E_DOT = "-e ."
 
-
-def get_requirements(file_path:str)->List[str]:
-    """
-    This function returns a list of requirements for the given files
-    Args:
-        file_path (str): It is the file path or the name of the file where all the requirements are stored.
-    Returns:
-        List[str]: It is the list of requirements.
+def get_requirements(file_path:str) -> List[str]:
+    """ 
+    This function will return the list of requirements which will be passed to the install_requires
+    which when further used to install requirements file.
+    
+    args: 
+    file_path: It is the path to requirements file or may be the name of requirements file 
+    output: It is the list of packages in requirements file 
     """
     requirements = []
     with open(file_path) as file_obj:
         requirements = requirements.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+        
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-    return requirements 
+    return requirements
 
-
-# Entering the metadata information for the project. 
+# This is the metadata information about the sales forcasting project
 setup(
-    name = 'Machien Learning end to end sales forcasting',
+    name = 'end to end sales forcasting machine learning with deployment',
     author = 'Jayesh',
     author_email = 'jayeshmandavkar5042@gmail.com',
     version = '0.0.1',
